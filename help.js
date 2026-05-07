93e96f0e859d92c22144a2d0f266485d10bdcb62
yr <189258527@qq.com>
2026-05-07 14:20:06 +0800

    Ignore CommonJS export in coverage

--- help.js
+++ help.js
@@ -9,6 +9,7 @@ function closeSidebar() {
     document.getElementById('sidebar').style.display = 'none';
 }
 
+/* istanbul ignore next */
 if (typeof module !== 'undefined') {
     module.exports = { openSidebar, closeSidebar };
 }
